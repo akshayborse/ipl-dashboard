d6a3cd6e4992e2fcbef2e1378711824649650f0b
Akshay Borse <akshay.borse29@gmail.com>
2021-05-08 12:57:27 +0530

    Complleted UI elements for Team Page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { TeamPage } from './pages/TeamPage';
+import { MatchPage } from './pages/MatchPage';
 
 function App() {
   return (
     <div className="App">
-      {/* <h1> IPL Dashboard</h1> */}
-      <TeamPage />
+      <Router>
+        <Switch>
+          <Route path="/teams/:teamName/matches/:year">
+            <MatchPage />
+          </Route>
+          <Route path="/teams/:teamName">
+            <TeamPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
