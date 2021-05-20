27c7f5a83be061ad2f17a544c042b92bf4f766dc
Akshay Borse <akshay.borse29@gmail.com>
2021-05-21 00:38:17 +0530

    Match Page year selector and HomePage

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 import { PieChart } from 'react-minimal-pie-chart';
@@ -13,14 +13,12 @@ export const TeamPage = () => {
 
     useEffect(
         () => {
-            const fetchMatches = async () => {
+            const fetchTeam = async () => {
                 const response = await fetch(`http://localhost:8080/team/${teamName}`);
                 const data = await response.json();
                 setTeam(data);
-                console.log(data)
-                console.log(team.teamName);
             };
-            fetchMatches();
+            fetchTeam();
         }, [teamName]
     );
 
@@ -47,7 +45,8 @@ export const TeamPage = () => {
             </div>
             {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match} />)}
             <div className="more-link">
-                <a href="#">More &gt;</a>
+                <Link to={`/teams/${teamName}/matches/${process.env.REACT_APP_DATA_END_YEAR}`}>More &gt;</Link>
+                <a href="#"></a>
             </div>
         </div>
     );
